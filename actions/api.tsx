9fba86cef34d2d24cab7a1aa90d31d6f7cd225c8
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-04-24 02:00:53 +0600

    organizing

--- actions/api.tsx
+++ actions/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL : "http://localhost:5000/api/"
+    baseURL : "https://realstate-dashboard-backend.vercel.app/api/"
 })
 
 // https://realstate-dashboard-backend.vercel.app/api/
