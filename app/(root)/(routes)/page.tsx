2353434d4da608caad936592547ff504b75f054f
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-04-24 12:39:15 +0600

    layout responsive

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -3,21 +3,21 @@
 import AccessProvider from "@/actions/accessProvider";
 import { Button } from "@/components/ui/button";
 import { getAllUsers } from "@/redux/auth/authSlice";
+import { PartyPopper } from "lucide-react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useSelector } from "react-redux";
 
 export default function Home() {
   AccessProvider()
+  const {role,user_name} = useSelector(({user} : any)=> user)
 
   return (
    <div>
-      <div className="h-[500px] w-[200px] bg-fuchsia-800">
-
-  </div>
-      <div className="h-[500px] w-[200px] bg-green-800">
-
-  </div>
+      <div>
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+        <h4 className="text-gray-500 text-sm">Welcome back, <span className="text-primary font-bold">{user_name}</span></h4>
+      </div>
    </div>
   );
 }
