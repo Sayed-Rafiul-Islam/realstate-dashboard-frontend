74fc891362e77ceacea8f5e09020470d7ce0ec1c
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-06-03 16:49:01 +0600

    notifications added

--- app/(root)/(routes)/(owner)/settings/invoice_type/components/client.tsx
+++ app/(root)/(routes)/(owner)/settings/invoice_type/components/client.tsx
@@ -4,17 +4,9 @@ interface InvoiceTypesClientProps {
     data : InvoiceTypeColumn[]
 }
 
-
 import { useEffect, useState } from "react"
-
-
-
 import { InvoiceTypeColumn, columns } from "./column"
 import { DataTable } from "@/components/ui/data-table"
-import { useSelector } from 'react-redux'
-import { PropertiesReducerProps, PropertyProps, UnitProps, UnitsReducerProps } from '@/types'
-import { Button } from '@/components/ui/button'
-import { DatePicker } from '@/components/ui/date-picker-form'
 
 export const InvoiceTypesClient : React.FC<InvoiceTypesClientProps> = ({data}) => {
 
