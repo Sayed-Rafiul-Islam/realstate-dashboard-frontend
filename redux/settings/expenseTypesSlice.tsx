71cc9e17950ffd545bd494d8ef7f0fbcfbec6775
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-06-06 14:59:15 +0600

    maintainer deashboard

--- redux/settings/expenseTypesSlice.tsx
+++ redux/settings/expenseTypesSlice.tsx
@@ -2,7 +2,7 @@
 import { ExpenseTypeProps } from "@/types"
 import {createSlice } from "@reduxjs/toolkit"
 
-const expenseTypesJson = typeof window !== "undefined" && localStorage.getItem("expenseTypes")
+const expenseTypesJson = typeof window !== "undefined" && window.window.localStorage.getItem("expenseTypes")
 
 const initialState = {
     expenseTypes : expenseTypesJson ? JSON.parse(expenseTypesJson) : [],
@@ -16,26 +16,26 @@ const expenseTypesSlice = createSlice({
     reducers : {
         getExpenseTypes : (state, {payload}) => {
             state.expenseTypes = payload
-            localStorage.removeItem("expenseTypes")
-            localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
+            window.localStorage.removeItem("expenseTypes")
+            window.localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
         },
         removeExpenseType : (state, {payload}) => {
             const temp = state.expenseTypes.filter(({_id} : ExpenseTypeProps) => _id !== payload._id)
             state.expenseTypes = temp
-            localStorage.removeItem("expenseTypes")
-            localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
+            window.localStorage.removeItem("expenseTypes")
+            window.localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
         },
         updateExpenseType : (state, {payload}) => {
             const index = state.expenseTypes.findIndex((item : ExpenseTypeProps) => item._id === payload._id)
             state.expenseTypes[index] = payload
-            localStorage.removeItem("expenseTypes")
-            localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
+            window.localStorage.removeItem("expenseTypes")
+            window.localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
         },
 
         addExpenseType : (state, {payload}) => {
             state.expenseTypes.push(payload)
-            localStorage.removeItem("expenseTypes")
-            localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
+            window.localStorage.removeItem("expenseTypes")
+            window.localStorage.setItem("expenseTypes", JSON.stringify(state.expenseTypes))
         },
     }
 })
