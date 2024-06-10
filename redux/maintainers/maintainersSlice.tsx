ecdebb6ac0dd8f76791c355d6cc519a87123e471
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-06-11 01:47:14 +0600

    maintainer

--- redux/maintainers/maintainersSlice.tsx
+++ redux/maintainers/maintainersSlice.tsx
@@ -16,50 +16,33 @@ const maintainersSlice = createSlice({
         getMaintainers : (state, {payload}) => {
             state.maintainers = payload
             if (typeof window !== 'undefined') {
-            localStorage.removeItem("maintainers")
-            localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
+                localStorage.removeItem("maintainers")
+                localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
             }
         },
         updateMaintainer : (state, {payload}) => {
-            const temp = state.maintainers.filter((item : MaintainerProps) => {
-                if (item._id === payload._id) {
-                    item.name = payload.name,
-                    item.email = payload.email,
-                    item.contactNo = payload.contactNo,
-                    item.type = payload.type
+            const index = state.maintainers.findIndex((item : MaintainerProps) => item._id === payload._id)
+            state.maintainers[index] = payload
+            if (typeof window !== 'undefined') {
+                    localStorage.removeItem("maintainers")
+                    localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
                 }
-                return item
-            })
-
-            console.log(temp)
-            state.maintainers = temp
-            localStorage.removeItem("tenants")
-            localStorage.setItem("tenants", JSON.stringify(state.maintainers))
         },
-
-        // addTenant : (state, {payload} ) => {
-        //     const {propertyFile, personalFile, ...rest} = payload 
-        //     const data = {...rest, _id : '5'}
-        //     state.tenants.push(data)
-        //     localStorage.removeItem("tenants")
-        //     localStorage.setItem("tenants", JSON.stringify(state.tenants))
-        // },
-
         addMaintainer : (state, {payload}) => {
             const data = {...payload,_id : '5'}
             state.maintainers.push(data)
             // console.log(state.maintainers)
             if (typeof window !== 'undefined') {
-            localStorage.removeItem("maintainers")
-            localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
+                localStorage.removeItem("maintainers")
+                localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
             }
         },
         removeMaintainer : (state, {payload}) => {
             const temp = state.maintainers.filter(({_id} : MaintainerProps) => _id !== payload._id)
             state.maintainers = temp
             if (typeof window !== 'undefined') {
-            localStorage.removeItem("maintainers")
-            localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
+                localStorage.removeItem("maintainers")
+                localStorage.setItem("maintainers", JSON.stringify(state.maintainers))
             }
         },
     }
